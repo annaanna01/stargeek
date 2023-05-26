216f2a3e98dcbfa9a8bd6a36e7aa78f4acda6906
Anna Laura <anna.martin@portalsesisp.org.br>
2023-05-26 17:16:49 -0300

    logica2605

---
+++ catalogo.js
@@ -0,0 +1,45 @@
+const cards = document.querySelector(".cards");
+
+carregarCatalogo();
+
+function carregarCatalogo(){
+    let dados = JSON.parse(localStorage.getItem("catalogo"));
+    let divcard = document.createElement("div");
+    divcard.setAttribute("class", "card")
+    if (dados == null){
+        divcard.innerHTML = "<p> Nenhum item encontrado </p>";
+        cards.appendChild(divcard);
+        return null;
+    }
+
+    dados.forEach((elemento, indice) => {
+        let divcard = document.createElement("div");
+        divcard.setAttribute("class", "card")
+        divcard.innerHTML = `
+        <img src ="img/${elemento.foto}">
+        <div class="info">
+        <img src="imagens/Mask group (2).png" alt="" onclick="editar(${indice})"></a>
+        <img src="imagens/lixeira.png" alt="" onclick="excluir(${indice})">
+        </div>
+        <p> ${elemento.nome}</p>
+    
+        `;
+        cards.appendChild(divcard);
+
+    });
+}
+
+function excluir(indice){
+    let dados = JSON.parse(localStorage.getItem("catalogo"));
+    dados.splice(indice,1);
+    localStorage.setItem("catalogo", JSON.stringify(dados));
+    window.location.reload();
+
+}
+
+function editar(indice){
+    var url ="cadastraritem.html?peditar=true&indice="+
+    encodeURIComponent(indice);
+    window.location.href = url;
+    
+}
